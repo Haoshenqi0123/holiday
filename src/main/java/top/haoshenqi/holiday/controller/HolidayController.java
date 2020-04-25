8ee92f3dd0b8d5ba1ca2fa137355a6e233f8028f
Haoshenqi0123 <shenqivpn@yahoo.com>
2020-04-25 16:08:35 +0800

    ADD 增加一个接口，直接查询当天是否是节假日

--- src/main/java/top/haoshenqi/holiday/controller/HolidayController.java
+++ src/main/java/top/haoshenqi/holiday/controller/HolidayController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.haoshenqi.holiday.model.HolidayDate;
@@ -17,7 +18,8 @@ import java.util.List;
  * @author haosh
  */
 @Slf4j
-@RestController("/holiday")
+@RestController
+@RequestMapping("/holiday")
 public class HolidayController {
     @Autowired
     HolidayDateService holidayDateService;
@@ -33,6 +35,24 @@ public class HolidayController {
                 date=DateUtil.getCurDate();
             }
             return holidayDateService.getHolidays(date);
+        }catch (Exception e){
+            log.info(e.getMessage());
+            log.error(e.getMessage(),e);
+            return null;
+        }
+    }
+    @GetMapping("/today")
+    public String getToday(){
+        try {
+
+            String curDate = DateUtil.getCurDate();
+            List<HolidayDate> holidays = holidayDateService.getHolidays(curDate);
+            Integer status = holidays.get(0).getStatus();
+            if(status%2==0){
+                return "工作";
+            }else {
+                return "休息";
+            }
         }catch (Exception e){
             log.info(e.getMessage());
             log.error(e.getMessage(),e);
